6d134209d7551c5e9fe6a11090ff733a92c6bb38
Matthew Sand <matthewsand22@gmail.com>
2021-02-21 14:26:59 -0500

    d a t a b a s e d pt2 electric boogaloo

--- retreiveDatabaseEntries.php
+++ retreiveDatabaseEntries.php
@@ -3,20 +3,15 @@ $servername = "mysql.matthewsand.info";
 $username = "wifi_panda_joe";
 $password = "wifi_panda_12";
 
-$conn = new mysqli($servername, $username, $password, "wifi_panda");
+$mysqli = new mysqli($servername, $username, $password, "wifi_panda");
 
-if (!$result = mysqli_query($conn, "SELECT * FROM hotspots")) {
-    printf("Error: %s\n", $mysqli->error);
-    exit;
+$query = "SELECT * FROM hotspots;";
+$result = $mysqli -> query($query);
+
+$outputJSON = '{"hotspots":[';
+while ($row=mysqli_fetch_row($result)) {
+    $outputJSON .= '{"id":"'.$row[0].'","locationName":"'.$row[1].'","lat":"'.$row[2].'","long":"'.$row[3].'","email":"'.$row[4].'","phone":"'.$row[5].'","website":"'.$row[6].'"},';
 }
-
-//$result = $conn->query("USE wifi_panda; select * from hotspots;") or die(mysqli_error()) ;
-// echo $result;
-$result = mysqli_query($conn, "SHOW TABLES");
-// $rows = array();
-//   while ($r = $result->fetch_assoc()) {
-//       $rows['object_name'][] = $r;
-//   }
-//
-// echo json_encode($rows);
-// //echo "am here";
+$finalOutput = rtrim($outputJSON, ", ");
+$finalOutput .= ']}';
+echo $finalOutput ;
