4609dfc10228ead570a5256763e2fbb264bd7fe3
Phuc <phucduongh@gmail.com>
2021-02-21 12:04:26 -0500

    add error handler for map

--- maps.js
+++ maps.js
@@ -1,9 +1,26 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  initMap(position.coords.latitude, position.coords.longitude);
-});
+getUserLocation();
 
-function initMap(latCoords, lngCoords) {
-  const mapCenter = { lat: latCoords, lng: lngCoords }
+function getUserLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mapLocation, error);
+  }
+  else {
+    alert("Sorry, please use a new device!");
+  }
+}
+
+function mapLocation(position) {
+  showMap(position.coords.latitude, position.coords.longitude);
+}
+
+function error(errorCode) {
+  if (errorCode.code == 1) {
+    alert("Please allow us to permission to your location in order to use our service!")
+  }
+}
+
+function showMap(latCoords, longCoords) {
+  const mapCenter = { lat: latCoords, lng: longCoords }
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: mapCenter,
